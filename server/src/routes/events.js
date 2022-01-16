24a2acb3aa56cb17e9edb8ed9d57743f94753203
Matija Janjic <matijanjic@gmail.com>
2022-01-16 22:41:07 +0000

    added the patch route for updating events

--- server/src/routes/events.js
+++ server/src/routes/events.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { getEvents, createEvent } = require('../services/eventService');
+const { getEvents, createEvent, updateEvent } = require('../services/eventService');
 
 router.get('/', async (req, res) => {
   const events = await getEvents();
@@ -8,9 +8,16 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
   const event = req.body;
-  console.log(event);
   const createdEvent = await createEvent(event);
   res.json(createdEvent);
+});
+
+router.patch('/:id', async (req, res) => {
+  const { id } = req.params;
+  const eventFieldsToUpdate = req.body;
+  console.log(eventFieldsToUpdate);
+  const updatedEvent = await updateEvent(id, eventFieldsToUpdate);
+  res.json(updatedEvent);
 });
 
 module.exports = router;
--- server/src/services/eventService.js
+++ server/src/services/eventService.js
@@ -2,19 +2,22 @@ const Event = require('../models/Event');
 
 const getEvents = async () => {
   const events = await Event.find({}).populate(['creator', 'includes']);
-  console.log('getEvents', events);
-
   return events;
 };
 
 const createEvent = async (event) => {
-  console.log('createEvent', event);
   const eventToSave = new Event(event);
   const savedEvent = await eventToSave.save();
   return savedEvent;
 };
 
+const updateEvent = async (id, eventFieldsToUpdate) => {
+  const updatedEvent = await Event.findByIdAndUpdate(id, eventFieldsToUpdate, { new: true });
+  return updatedEvent;
+};
+
 module.exports = {
   getEvents,
   createEvent,
+  updateEvent,
 };
