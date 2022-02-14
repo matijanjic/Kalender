7e3a278df22cf99c048044ca18614f5deabad6fd
Matija Janjic <matijanjic@gmail.com>
2022-02-14 23:30:25 +0000

    fullBuild

--- server/src/index.js
+++ server/src/index.js
@@ -55,7 +55,7 @@ if (!isDev && cluster.isMaster) {
   // All remaining requests return the React app, so it can handle routing.
   app.get('*', (_request, response) => {
     response.sendFile(
-      path.resolve(__dirname, '../react-ui/build', 'index.html'),
+      path.resolve(__dirname, '../../client/build', 'index.html'),
     );
   });
 
